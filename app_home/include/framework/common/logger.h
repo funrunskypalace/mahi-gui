f9c179396a906ae3e6189c9b1286260b54dc4671
jiangsy_futures <sanyi0127@sina.com>
2021-02-08 10:20:30 +0800

    update consoleLevel name

--- app_home/include/framework/common/logger.h
+++ app_home/include/framework/common/logger.h
@@ -3,30 +3,26 @@
 #include "fmt/format.h"
 #include "framework/common/common.h"
 
-namespace framework
-{
+namespace framework {
 
-enum level_enum
-{
-    trace = 0,
-    debug = 1,
-    info = 2,
-    warn = 3,
-    err = 4,
+enum level_enum {
+    trace    = 0,
+    debug    = 1,
+    info     = 2,
+    warn     = 3,
+    err      = 4,
     critical = 5,
-    off = 6,
+    off      = 6,
 };
 
 /**
  * Automatically created logger for each strategy instance.
  */
-class GreatWallLogger
-{
+class GreatWallLogger {
 public:
     GreatWallLogger() = default;
     template <typename... Args>
-    void log(int level, const char* fmt, const Args&... args)
-    {
+    void log(int level, const char* fmt, const Args&... args) {
         log_(level, fmt::format(fmt, args...));
     }
 
@@ -36,9 +32,7 @@ public:
 using GreatWallLoggerPtr = std::shared_ptr<GreatWallLogger>;
 
 // has module name: "[%Y-%m-%d %T.%e][%t][%l][%n] %v"
-extern void greatwall_init_global_log(const char* logPathName,
-                                      const char* loggerName, int logLevel,
-                                      bool enableConsole,
-                                      const std::string& logFormat);
+extern void greatwall_init_global_log(const char* logPathName, const char* loggerName, int logLevel,
+                                      int consoleLevel, const std::string& logFormat);
 
-} // namespace framework
+}  // namespace framework
